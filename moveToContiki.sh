0d3bfd7f24286e794407ee9689c88a6cdc949971
Suhaib <skm20@ic.ac.uk>
2024-05-14 15:20:15 +0100

    added script to move to main contiki folder for ease

---
+++ moveToContiki.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm ../contiki-ng/tools/cooja/java/org/contikios/cooja/plugins/SimulationControlPlugin.java
+cp ./java/org/contikios/cooja/plugins/SimulationControlPlugin/SimulationControlPlugin.java ../contiki-ng/tools/cooja/java/org/contikios/cooja/plugins/SimulationControlPlugin.java
